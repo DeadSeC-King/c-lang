c2578a9ae90b32a0d4d1d29252496c3ab57bd2e0
Kushagra Ranjan <kushagraranjankr@gmail.com>
2025-09-21 09:58:39 +0530

    done

---
+++ 100d31q2.c
@@ -0,0 +1,13 @@
+//Merge two arrays.
+#include <stdio.h>
+#include <stdlib.h>
+void merge(int arr1[], int arr2[], int n1, int n2, int arr3[])
+{
+    int i = 0, j = 0, k = 0;
+    while (i < n1) {
+        arr3[k++] = arr1[i++];
+    }
+    while (j < n2) {
+        arr3[k++] = arr2[j++];
+    }
+}
